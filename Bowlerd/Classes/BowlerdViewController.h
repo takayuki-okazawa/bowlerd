a247fc8049926b3f67f4fd0fd20c1f3ef3b72c1f
takayuki <user@Takayuki-Mac-Mini.local>
2013-02-07 08:53:10 +0900

    add file

---
+++ Bowlerd/Classes/BowlerdViewController.h
@@ -0,0 +1,16 @@
+//
+//  BowlerdViewController.h
+//  Bowlerd
+//
+//  Created by takayuki on 10/10/15.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BowlerdViewController : UIViewController {
+
+}
+
+@end
+
